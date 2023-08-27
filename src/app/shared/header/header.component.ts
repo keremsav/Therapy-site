0b71917fd54592be42b151e6428449491328b035
keremsav <krmsv19@gmail.com>
2023-08-27 18:28:17 +0300

    Header fixed. Counsel section img fixed.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,21 +14,7 @@ export class HeaderComponent {
 
   constructor(private renderer: Renderer2) {}
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollY = window.scrollY;
-    const header = document.querySelector('.header');
 
-    if (!this.isMenuOpen) {
-        if (scrollY > 0) {
-            this.renderer.addClass(header, 'blurred');
-            this.isHeaderBlurred = true;
-        } else {
-            this.renderer.removeClass(header, 'blurred');
-            this.isHeaderBlurred = false;
-        }
-    }
-  }
 
 
   toggleDropdown() {
